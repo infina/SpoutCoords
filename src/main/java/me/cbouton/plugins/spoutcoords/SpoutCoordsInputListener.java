e30599a4da010db281a6c67a4c312a30aa058ca9
Furt <lit_69@hotmail.com>
2011-09-29 23:00:17 -0500

    Added permissions node "spoutcoords.toggle" and cleaned up more code.

--- src/main/java/me/cbouton/plugins/spoutcoords/SpoutCoordsInputListener.java
+++ src/main/java/me/cbouton/plugins/spoutcoords/SpoutCoordsInputListener.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package me.cbouton.plugins.spoutcoords;
 
 import org.getspout.spoutapi.event.input.InputListener;
@@ -10,38 +6,25 @@ import org.getspout.spoutapi.keyboard.Keyboard;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 /**
- *
+ * 
  * @author Corey
  */
 class SpoutCoordsInputListener extends InputListener {
-    private final Spoutcoords plugin;
-    
-    public SpoutCoordsInputListener(Spoutcoords plugin){
-        this.plugin = plugin;
-    }
+	private final Spoutcoords plugin;
 
-    @Override
-    public void onKeyPressedEvent(KeyPressedEvent event) {
-        if(event.getKey() == Keyboard.KEY_F4){
-            SpoutPlayer player = event.getPlayer();
-            plugin.setCoords(player, !plugin.hasCoords(player));
-            System.out.println(player + "has toggled coordinates.");
-        }
-    }
+	public SpoutCoordsInputListener(Spoutcoords plugin) {
+		this.plugin = plugin;
+	}
 
-   /* @Override
-    public void onKeyPressedEvent(KeyPressedEvent event) {
-        Keyboard keypress = event.getKey();
-        System.out.println(keypress.toString());
-        SpoutPlayer player = event.getPlayer();
-        if(keypress.compareTo(Keyboard.KEY_F4) == 0){
-            plugin.setCoords(player, !plugin.hasCoords(player));
-            System.out.println(player + "has enabled coordinates.");
-            return;
-        }
-    }*/
-    
-
-    
-    
+	@Override
+	public void onKeyPressedEvent(KeyPressedEvent event) {
+		if (event.getKey() == Keyboard.KEY_F4) {
+			if (event.getPlayer().hasPermission("spoutcoords.toggle")) {
+				SpoutPlayer player = event.getPlayer();
+				plugin.setCoords(player, !plugin.hasCoords(player));
+				Spoutcoords.log.info(player.getName()
+						+ " has toggled coordinates.");
+			}
+		}
+	}
 }
